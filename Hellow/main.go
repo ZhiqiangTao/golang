6035618722570e8534a9a461e8fb0ac8a8bd4f51
ZhiqiangTao <13611774226@163.com>
2021-08-26 13:57:27 +0800

    123

---
+++ Hellow/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	p1 "golearn/package1"
+)
+
+func main() {
+	fmt.Println(p1.Age)
+}
---
+++ Hellow/package1/calc.go
@@ -0,0 +1,9 @@
+package package1
+
+import "fmt"
+
+const Age = 18
+
+func init() {
+	fmt.Printf("package1 init执行了")
+}
