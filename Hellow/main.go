51fdcbe4579fec82e000b67f47b74bfe835447c5
ZhiqiangTao <13611774226@163.com>
2021-08-31 16:31:21 +0800

    123

--- Hellow/main.go
+++ Hellow/main.go
@@ -38,12 +38,91 @@ func traversalString() {
 	fmt.Println()
 }
 
+//a : 1 b:2 c:3
+func f1(a, b int, c bool) int {
+	if c {
+		return a + b
+	} else {
+		return a - b
+	}
+}
+
+/*
+	123
+	123
+	123
+*/
+func f2() {
+	fmt.Println("1")
+	defer fmt.Println("2")
+	fmt.Println("3")
+	defer fmt.Println("4")
+}
+
+func f3() int {
+	x := 5
+	defer func(aa int) {
+		aa++
+	}(x)
+	return x
+}
+
+func calc(index string, a, b int) int {
+	ret := a + b
+	fmt.Println(index, a, b, ret)
+	return ret
+}
+
 func main() {
-	s1 := "白萝卜"
-	s2 := []rune(s1)
-	fmt.Printf("%T\n", s2)
-	s2[0] = '红'
-	fmt.Println(string(s2))
+	a, b := 1, 2
+	defer calc("1", a, calc("10", a, b))
+	a = 0
+	defer calc("2", a, calc("20", a, b))
+	b = 1
+
+	//fmt.Println(f3())
+
+	// fmt.Println(f1(1, 2, false))
+
+	// n := 18
+	// fmt.Println(&n)
+	// var a1 = [...]int{3,7,8,9,1}
+	// sort.Ints(a1[:])
+	// fmt.Println(a1)
+
+	// s1 := []string{"A", "B"}
+	// fmt.Println(len(s1), cap(s1))
+	// s1 = append(s1, "C")
+	// fmt.Println(len(s1), cap(s1))
+	// s2 := append(s1, "D")
+	// fmt.Println(len(s1), cap(s1))
+	// fmt.Println(len(s2), cap(s2))
+
+	// a1 := [...]int{1, 3, 5, 7, 9, 11, 13}
+	// s3 := a1[:4]
+	// fmt.Printf("%T", s3)
+
+	// var s1 = []string{"A", "B", "C"}
+	// fmt.Printf("type:%T len(s1):%d cap(s1):%d", s1, len(s1), cap(s1))
+
+	// var s []int
+	// fmt.Printf("%T\n", s)
+	// fmt.Println(s == nil)
+
+	// var a1 [3]int32
+	// var a2 [4]bool
+	// fmt.Printf("a1:%T a2:%T", a1, a2)
+
+	// flag := "我是中国人"
+	// var a int32
+	// a = int32(flag)
+	// fmt.Println(a)
+
+	// s1 := "白萝卜"
+	// s2 := []rune(s1)
+	// fmt.Printf("%T\n", s2)
+	// s2[0] = '红'
+	// fmt.Println(string(s2))
 
 	// traversalString()
 
